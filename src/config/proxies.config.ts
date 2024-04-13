c50d5f1153fce280a8f02164f12030515632858a
Jad <jadob2003@hotmail.com>
2024-04-12 20:36:12 -0400

    moved proxies to env

--- src/config/proxies.config.ts
+++ src/config/proxies.config.ts
@@ -1,5 +1,4 @@
 import { registerAs } from "@nestjs/config";
-import proxies from "proxies.json";
 export default registerAs("proxies", () => ({
-  proxies: proxies ?? []
+  proxies: JSON.parse(process.env.PROXIES || "[]")
 }));
